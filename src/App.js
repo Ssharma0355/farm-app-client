43c2d5e00a8c8238b87452ebfdf11f4d05231927
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-10-31 18:40:26 +0530

    checked apis into frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [data, setData] = useState("")
+  const getData = () =>{
+    axios.get("http://127.0.0.1:8000/")
+    .then((res)=>setData(res.data.message))
+    .catch((error) => console.error(error))
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+  
   return (
     <div>
-     <h1>Initial </h1>
+     <h1>{data} </h1>
     </div>
   )
 }
